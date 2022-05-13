af94ac9a524d8e8f7fc5b5bb9de7447383265aad
Tiia Kansa <como2e2@gmail.com>
2022-05-13 14:12:46 -0400

    Added functionality for Employees for getAll, getById, and Delete. Added a little UI

--- src/app/interfaces/employee.ts
+++ src/app/interfaces/employee.ts
@@ -2,4 +2,26 @@ export interface Employee {
     id: string;
     firstName: string;
     lastName: string;
+    address: {
+        id: string;
+        street: string;
+        city: string;
+        region: string;
+        postal: string;
+    }
+    companyEmail: string;
+    birthDate: string;
+    hiredDate: string;
+    role: string;
+    skills: Skill[];
+}
+
+interface Skill {
+    id: string;
+    field: {
+        id: string;
+        name: string;
+        type: string;
+    },
+    experience: number;
 }
